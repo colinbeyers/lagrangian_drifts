c5dbad7017f29fb2c486232154d7cb0f2c30a131
Colin Beyers <colin_beyers@mines.edu>
2024-10-14 13:24:46 -0600

    adds comments to fieldset_vars to make their purpose more clear

--- execute/fieldset_vars.py
+++ execute/fieldset_vars.py
@@ -3,8 +3,14 @@ class FieldsetVariable:
         self.name = name
         self.eval_func = eval_func
 
+    # A method that can be called on a FieldsetVarible to
+    # compute the eval function at runtime (after the fieldset
+    # object has been created)
     def value(self, fieldset):
         return self.eval_func(fieldset)
 
+    # This method returns the custom string for the 
+    # FielsetVariable object and is similarly worded to 
+    # other object strings in PARCELS (for debugging and logging)
     def __repr__(self):
         return f"<FieldsetVariable {self.name}>"
