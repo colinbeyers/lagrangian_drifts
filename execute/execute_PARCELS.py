2c37be4366316fc45c684a97eff5a59f9c2e1c27
Colin Beyers <colin_beyers@mines.edu>
2024-10-14 13:13:04 -0600

    restructures execute_PARCELS to work with new config file structure

--- execute/execute_PARCELS.py
+++ execute/execute_PARCELS.py
@@ -1,31 +1,43 @@
-import json
-import parcels
-from datetime import timedelta
-from fieldset_vars import FieldsetVariable
-import kernels
+import sys
+import os
 import logging
+
+import json
+
+from   datetime import timedelta
 import time
+
 import numpy as np
 import xarray as xr
-import coastal_boundary
 
-# Configure logging to write to a file
+import parcels
+import coastal_boundary
+import kernels
+from   fieldset_vars import FieldsetVariable
+
+# Temporary log-to-consol configuration
 logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s',
-    handlers=[
-        logging.FileHandler("model.log"),  # Save logs to simulation.log
-        logging.StreamHandler()  # Also print logs to console
-    ]
+    level       = logging.INFO,
+    format      = '%(asctime)s - %(levelname)s - %(message)s',
+    handlers    = [logging.StreamHandler()]
 )
 
-# Function to load the configuration file
-def load_config(config_path="config.json"):
-    logging.info("Loading configuration from %s", config_path)
+def load_config(config_path=None):
+    if config_path is None:
+        logging.error("No configuration file path provided.")
+        sys.exit(1)
+    
+    logging.info(f"Loading configuration from {config_path}")
+    
+    if not os.path.exists(config_path):
+        logging.error(f"Configuration file {config_path} does not exist.")
+        sys.exit(1)
+    
     with open(config_path, "r") as f:
-        return json.load(f)
+        config = json.load(f)
 
-# Function to collect extra variables required by the kernels
+    return config
+
 def collect_extra_vars(kernel_list):
     extra_vars = []
     for kernel in kernel_list:
@@ -39,80 +51,112 @@ def collect_extra_vars(kernel_list):
     logging.info("Collected extra variables for kernels: %s", extra_vars)
     return extra_vars
 
-# Function to dynamically load kernels from the config
 def load_kernels(kernel_names, parcels, kernels):
+    """
+    Checks to see if the called kernels exist and then adds it to the list.
+    """
     kernel_list = []
+
+    # check if each kernel exists in PARCELS or custom file
     for kernel_name in kernel_names:
         if hasattr(parcels, kernel_name):
             kernel_list.append(getattr(parcels, kernel_name))
         elif hasattr(kernels, kernel_name):
             kernel_list.append(getattr(kernels, kernel_name))
         else:
-            raise ValueError(f"Kernel {kernel_name} not found in parcels or kernels.py.")
-    logging.info("Loaded kernels: %s", kernel_names)
+            logging.error(f"Kernel {kernel_name} not found in PARCELS or kernels.py.")
+            sys.exit(1)
+
+    logging.info(f"Loaded kernels: {kernel_names}")
+
     return kernel_list
 
-def create_fieldset(config):
-    if config.get("variable_names", {}):
-        vars = config["variable_names"]
+def create_fieldset(config, extra_vars):
+    vars = config["data"]["var_names"]
+    dims = config["data"]["dim_names"]
     
-    if config.get("dimension_names", {}):
-        dims = config["dimension_names"]
-    
-    if config.get("coastal_boundary", {}).get("include", False):
+    if config["setup"]["coastal_boundary"]["include"] == True:
         logging.info("Including a coastal boundary.")
 
-        if not config.get("coastal_boundary", {}).get("path", "") == None:
-            logging.warning("Coastal boundary has not yet been added to the main dataset. Adding...")
-            ds = xr.open_dataset(config["coastal_boundary"]["path"])
-            ds_boundary = xr.open_dataset(config["coastal_boundary"]["path"])
-            ds_combined = coastal_boundary.add_datasets(ds, ds_boundary)
+        if config["setup"]["coastal_boundary"]["path"] != None:
+            logging.warning("Coastal boundary has not been added to the main dataset. Adding...")
+            ds          = xr.open_dataset(config["data"]["path"])
+            boundary    = xr.open_dataset(config["setup"]["coastal_boundary"]["path"])
+            ds_boundary = coastal_boundary.dimmentionalize(ds, boundary)
+            ds_combined = ds + ds_boundary
+            ds_combined = ds_combined.sel(time=slice(config["setup"]["runtime"]["start"], None))
 
             fieldset = parcels.FieldSet.from_xarray_dataset(ds_combined, vars, dims)
-            logging.info("Fieldset created with filenames: %s", config["coastal_boundary"]["path"])
+            logging.info(f"Fieldset created with filenames: {config["data"]["path"]}, "
+                         + f"{config["setup"]["coastal_boundary"]["path"]}")
             
-        if config.get("coastal_boundary", {}).get("path", "") == None:
-            logging.warning("No coastal boundary path provided. Assuming main path includes the coastal boundary already.")
+        if config["setup"]["coastal_boundary"]["path"] == None:
+            logging.warning("No coastal boundary path provided."
+                            + "Assuming main dataset includes the coastal boundary already.")
+            
+            ds          = xr.open_dataset(config["data"]["path"])
+            ds          = ds.sel(time=slice(config["setup"]["runtime"]["start"], None))
+            fieldset    = parcels.FieldSet.from_xarray_dataset(ds, vars, dims)
 
-            fieldset = parcels.FieldSet.from_netcdf(config["pathname"], vars, dims)
-            logging.info("Fieldset created with filenames: %s", config["pathname"])
+            logging.info(f"Fieldset created with filenames: {config["data"]["path"]}")
     else:
-        logging.info("Not including a coastal boundary.")
+        logging.warning("Not including a coastal boundary.")
 
-        fieldset = parcels.FieldSet.from_netcdf(config["pathname"], vars, dims)
-        logging.info("Fieldset created with filenames: %s", config["pathname"])
+        ds          = xr.open_dataset(config["data"]["path"])
+        ds          = ds.sel(time=slice(config["setup"]["runtime"]["start"], None))
+        fieldset    = parcels.FieldSet.from_xarray_dataset(ds, vars, dims)
+
+        logging.info(f"Fieldset created with filenames: {config["data"]["path"]}")
+
+    # Add extra fieldset type variables to fieldset object
+    for extra_var in extra_vars:
+        if isinstance(extra_var, FieldsetVariable):
+            fieldset.add_constant(extra_var.name, extra_var.value(fieldset))
+        else:
+            fieldset.add_constant(extra_var.name, extra_var)
 
     return fieldset
 
-def load_coords(config):
+def load_particle_locs(config):
     """Load longitude and latitude lists from specified text files in the config."""
-    lat_file = config["particles"]["lat_file"]
-    lon_file = config["particles"]["lon_file"]
+    lat_file = config["setup"]["particles"]["lat_file"]
+    lon_file = config["setup"]["particles"]["lon_file"]
 
     try:
         # Read the latitude values
-        lats = np.loadtxt(lat_file).tolist()  # Convert to list
-        logging.info(f"Loaded latitudes: {lats}")
+        lats = np.loadtxt(lat_file).tolist()
+        logging.info(f"Loaded latitudes from {lat_file}.")
 
         # Read the longitude values
-        lons = np.loadtxt(lon_file).tolist()  # Convert to list
-        logging.info(f"Loaded longitudes: {lons}")
+        lons = np.loadtxt(lon_file).tolist()
+        logging.info(f"Loaded longitudes from {lon_file}")
 
         return lats, lons
-    except Exception as e:
-        logging.error(f"Error loading coordinates: {e}")
-        return [], []
+    
+    except Exception as exc:
+        logging.error(f"Error loading coordinates: {exc}")
+        sys.exit(1)
+
+def setup_particles_class(extra_vars):
+    extra_particle_vars = []
+    for extra_var in extra_vars:
+        if type(extra_var) == parcels.particle.Variable:
+            extra_particle_vars.append(extra_var)
+    particles = parcels.JITParticle.add_variables(extra_particle_vars)
+
+    logging.info(f"Partical class initialized successfully with extra variables {extra_particle_vars}.")
+    return particles
 
 def create_particle_set(fieldset, particle_class, config):
     """Create a ParticleSet with initial locations from config."""
     # Load lons and lats from the specified files
-    lats, lons = load_coords(config)
+    lats, lons = load_particle_locs(config)
     
     if not lons or not lats:
         logging.error("No valid coordinates found. ParticleSet cannot be created.")
         return None  # Return None or raise an exception if coordinates are invalid
 
-    logging.info("Creating ParticleSet with initial locations: lon=%s, lat=%s",
+    logging.info("Creating ParticleSet...",
                  lons, lats)
 
     return parcels.ParticleSet(
@@ -122,63 +166,57 @@ def create_particle_set(fieldset, particle_class, config):
         lat=lats
     )
 
-# Function to set up the ParticleFile for output
 def setup_output_file(pset, config):
-    output_dir = config["output"]["directory"]
-    file_name = config["output"]["file_name"]
-    output_interval = timedelta(hours=config["output"].get("output_interval_hours", 6))
+    output_dir      = config["model"]["path"] + 'output/'
+    file_name       = config["model"]["name"] + ".zarr"
+    output_interval = timedelta(hours=config["output"]["interval_in_hours"])
 
-    logging.info("Setting up output file: %s/%s with interval %s", output_dir, file_name, output_interval)
+    logging.info(f"Setting up output file {output_dir}{file_name} with interval {output_interval}.")
     return pset.ParticleFile(name=f"{output_dir}/{file_name}", outputdt=output_interval)
 
-# Main function to execute the simulation
 def main():
 
-    # load the model configuration
-    config = load_config()
+    # pass the second command line arg to load_config()
+    config = load_config(sys.argv[1] if len(sys.argv) > 1 else None)
 
-    # Load the kernels dynamically based on the config
-    kernel_list = load_kernels(config.get("kernels", []), parcels, kernels)
+    # initialize the main log after loading the config file
+    for handler in logging.root.handlers[:]:
+        logging.root.removeHandler(handler)
+    log_path = config["model"]["path"] + config["model"]["name"] + ".log"
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        handlers=[
+            logging.FileHandler(log_path),
+            logging.StreamHandler()
+        ]
+    )
 
-    # Collect the extra variables required by the kernels
-    extra_vars = collect_extra_vars(kernel_list)
+    logging.info(f"Configuration {sys.argv[1]} loaded successfully.")
 
-    # Add extra particle type variables to particle class
-    extra_particle_vars = []
-    for extra_var in extra_vars:
-        if type(extra_var) == parcels.particle.Variable:
-            extra_particle_vars.append(extra_var)
-    particles = parcels.JITParticle.add_variables(extra_particle_vars)
-
-    # Create the fieldset based on the provided config
-    fieldset = create_fieldset(config)
-
-    # Add extra fieldset type variables to fieldset object
-    for extra_var in extra_vars:
-        if isinstance(extra_var, FieldsetVariable):
-            fieldset.add_constant(extra_var.name, extra_var.value(fieldset))
-        else:
-            fieldset.add_constant(extra_var.name, extra_var)
-
-    # Create the ParticleSet based on the fieldset object and particle class
-    pset = create_particle_set(fieldset, particles, config)
-
-    # Set up the output file
-    output_file = setup_output_file(pset, config)
+    kernels_list    = load_kernels(config["setup"]["kernels"], parcels, kernels)
+    extra_vars      = collect_extra_vars(kernels_list)
+    particles       = setup_particles_class(extra_vars)
+    fieldset        = create_fieldset(config, extra_vars)
+    pset            = create_particle_set(fieldset, particles, config)
+    output_file     = setup_output_file(pset, config)
 
     # Execute the simulation and measure time
     start_time = time.time()
     logging.info(f"Starting the simulation at {start_time}...")
     
     pset.execute(
-        kernel_list,
-        runtime=timedelta(days=config["runtime"].get("days", 10)),
-        dt=timedelta(minutes=config["runtime"].get("dt_minutes", 5)),
-        output_file=output_file,
+        kernels_list,
+        runtime     = timedelta(days=config["setup"]["runtime"]["days"]),
+        dt          = timedelta(minutes=config["setup"]["runtime"]["dt_in_minutes"]),
+        output_file = output_file,
     )
 
     end_time = time.time()
-    logging.info(f"Simulation completed in {end_time - start_time} seconds")
+    elapsed_time = end_time - start_time
+    minutes, seconds = divmod(elapsed_time, 60)
+    logging.info(f"Simulation completed in {int(minutes)} minutes, {seconds:.1f} seconds")
+
 
 # Check if the script is being run directly
 if __name__ == "__main__":
